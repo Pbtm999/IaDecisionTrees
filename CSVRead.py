fad623df8e3bc2bb66036e2632f90d11b6042393
DiogoManim <diogomanimvila@gmail.com>
2024-04-29 12:04:46 +0100

    CSVRead w/pandas

--- CSVRead.py
+++ CSVRead.py
@@ -4,11 +4,9 @@ class CSVRead():
 
     def __init__(self, filename):
         self.trainData = pd.read_csv('datasets/'+filename+'.csv')
-        print(self.trainData.head())
 
     def getLine(self, line):
-        print(self.trainData.iloc[[line]])
-        return self.trainData.head(line)
+        return self.trainData.iloc[[line]]
     
     def getAllData(self):
         return self.trainData
